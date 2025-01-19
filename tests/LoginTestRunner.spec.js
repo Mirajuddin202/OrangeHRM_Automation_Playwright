17a0f46dd7269df62d8428f98ab4aef1e0946a33
Md Miraj Uddin <miraj.cse202002044@gmail.com>
2025-01-19 20:33:32 +0600

    Added Employee and Logout

--- tests/LoginTestRunner.spec.js
+++ tests/LoginTestRunner.spec.js
@@ -1,11 +1,21 @@
-import { test, expect } from "@playwright/test";
-import LoginPage from "../pages/LoginPage";
+import { test, expect } from '@playwright/test';
 
-test("Admin can login successfully", async ({ page }) => {
-  const login = new LoginPage(page);
-
-  await login.gotoLoginPage();
-  await login.login("Admin", "admin123");
-  await expect(page).toHaveURL(/dashboard/);
-  await expect(login.profilePic).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('admin');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'PIM' }).click();
+  await page.getByRole('link', { name: 'Add Employee' }).click();
+  await page.getByPlaceholder('First Name').click();
+  await page.getByPlaceholder('First Name').fill('miraj');
+  await page.getByPlaceholder('Last Name').click();
+  await page.getByPlaceholder('Last Name').fill('uddin');
+  await page.getByRole('button', { name: 'Save' }).click();
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/185');
+  await expect(page.getByRole('heading', { name: 'miraj uddin' })).toBeVisible();
+  await page.getByText('Ashish Rawat').click();
+  await page.getByRole('menuitem', { name: 'Logout' }).click();
 });
